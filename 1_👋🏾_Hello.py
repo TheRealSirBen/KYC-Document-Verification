f7b5bed692ecd8be3a33d08f994f686d1bdc1178
SirBen <thabisaben@gmail.com>
2024-01-26 13:12:26 +0200

    Update

--- 1_👋🏾_Hello.py
+++ 1_👋🏾_Hello.py
@@ -3,28 +3,20 @@ from init import initialize_database
 
 initialize_database()
 
-st.header("CHINHOYI UNIVERSITY OF TECHNOLOGY")
-
-
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('logo.jpeg', width=300)
+    st.image('logo.png', width=300)
 
 with col2:
     st.markdown(
         """
-                ### Student Details
+                ### Group Details
     
-                - Name: Benedict T. Dlamini
-                - Student Number: C22148273B
-                - Programme: MSC. Big Data Analytics
+                Name: 🧐 DataVision
     
                 ### Topic
                 A Deep Learning Approach to KYC Document Verification for the Customer 
-                Registration in Zimbabwean Financial Institutions: Case of Old Mutual Zimbabwe
+                Registration in Zimbabwean Financial Institutions
             """
     )
-
-st.write('### :blue[Project Description and How to navigate the App]')
-st.video('https://youtu.be/7s9RfaAPvEM')
